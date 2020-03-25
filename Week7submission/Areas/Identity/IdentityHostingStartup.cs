0332b6a0cd27a17cadc20e5156aefc3198f50209
Baljinder Singh Baljinder Singh <Baljindersingh4726@conestogac.on.ca>
2020-03-25 18:24:21 -0400

    Add project files.

---
+++ Week7submission/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Week7submission.Data;
+
+[assembly: HostingStartup(typeof(Week7submission.Areas.Identity.IdentityHostingStartup))]
+namespace Week7submission.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<UseridentyContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("UseridentyContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<UseridentyContext>();
+            });
+        }
+    }
+}
---
+++ Week7submission/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Week7submission.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
